6526b2ee78c1776735185cd978cfddc9680af42b
Blake Embrey <hello@blakeembrey.com>
2017-03-14 16:52:36 -0700

    Hide `stack` from tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,7 +4,7 @@ import { errorhandler } from './index'
 describe('servie-errorhandler', () => {
   it('should render a plain response', () => {
     const req = new Request({ url: '/' })
-    const handler = errorhandler(req)
+    const handler = errorhandler(req, { production: true })
     const res = handler(new Error('boom!'))
 
     expect(res).toMatchSnapshot()
@@ -12,7 +12,7 @@ describe('servie-errorhandler', () => {
 
   it('should render a html response', () => {
     const req = new Request({ url: '/', headers: { 'Accept': 'text/html' } })
-    const handler = errorhandler(req)
+    const handler = errorhandler(req, { production: true })
     const res = handler(new Error('boom!'))
 
     expect(res).toMatchSnapshot()
@@ -20,15 +20,7 @@ describe('servie-errorhandler', () => {
 
   it('should render a json response', () => {
     const req = new Request({ url: '/', headers: { 'Accept': 'application/json' } })
-    const handler = errorhandler(req)
-    const res = handler(new Error('boom!'))
-
-    expect(res).toMatchSnapshot()
-  })
-
-  it('should not render stack in production', () => {
-    const req = new Request({ url: '/', headers: { 'Accept': 'application/json' } })
-    const handler = errorhandler(req, { production: false })
+    const handler = errorhandler(req, { production: true })
     const res = handler(new Error('boom!'))
 
     expect(res).toMatchSnapshot()
