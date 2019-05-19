85718cec9888ffada5c045c07cf9319adbd54516
Blake Embrey <hello@blakeembrey.com>
2019-05-18 23:07:51 -0700

    Refactor package to support servie 4.x

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,33 +1,33 @@
-import { Request } from 'servie'
-import * as boom from 'boom'
-import * as httpErrors from 'http-errors'
-import { errorhandler } from './index'
+import { Request } from "servie/dist/node";
+import * as boom from "boom";
+import * as httpErrors from "http-errors";
+import { errorhandler } from "./index";
 
-describe('servie-errorhandler', () => {
-  const req = new Request({ url: '/' })
-  const basicHandler = errorhandler(req)
+describe("servie-errorhandler", () => {
+  const req = new Request("/");
+  const basicHandler = errorhandler(req);
 
   it('should fail "gracefully" with non-error', () => {
-    const res = basicHandler('test')
+    const res = basicHandler("test");
 
-    expect(res).toMatchSnapshot()
-  })
+    expect(res).toMatchSnapshot();
+  });
 
-  it('should render an error', () => {
-    const res = basicHandler(new Error('boom!'))
+  it("should render an error", () => {
+    const res = basicHandler(new Error("boom!"));
 
-    expect(res).toMatchSnapshot()
-  })
+    expect(res).toMatchSnapshot();
+  });
 
-  it('should render boom status errors', () => {
-    const res = basicHandler(boom.badRequest('data has an issue'))
+  it("should render boom status errors", () => {
+    const res = basicHandler(boom.badRequest("data has an issue"));
 
-    expect(res).toMatchSnapshot()
-  })
+    expect(res).toMatchSnapshot();
+  });
 
-  it('should render http errors status error', () => {
-    const res = basicHandler(new httpErrors.BadRequest('data has an issue'))
+  it("should render http errors status error", () => {
+    const res = basicHandler(new httpErrors.BadRequest("data has an issue"));
 
-    expect(res).toMatchSnapshot()
-  })
-})
+    expect(res).toMatchSnapshot();
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,103 +1,110 @@
-import { STATUS_CODES } from 'http'
-import escapeHtml = require('escape-html')
-import { Request, Response } from 'servie'
-import { sendJson, sendHtml } from 'servie-send'
-import Negotiator = require('negotiator')
+import { STATUS_CODES } from "http";
+import escapeHtml = require("escape-html");
+import Negotiator = require("negotiator");
+import { Request, Response } from "servie/dist/node";
+import { sendJson, sendHtml } from "servie-send";
 
-const DOUBLE_SPACE_REGEXP = /\x20{2}/g
+const DOUBLE_SPACE_REGEXP = /\x20{2}/g;
 
 export interface Options {
-  production?: boolean
-  log?: (err: any) => void
+  production?: boolean;
+  log?: (err: any) => void;
 }
 
 /**
  * Render errors into a response object.
  */
-export function errorhandler (req: Request, options: Options = {}): (err: any) => Response {
-  const env = process.env.NODE_ENV || 'development'
-  const production = options.production === true || env === 'production'
-  const log = options.log || (env === 'test' ? Function.prototype : console.error)
+export function errorhandler(
+  req: Request,
+  options: Options = {}
+): (err: any) => Response {
+  const env = process.env.NODE_ENV || "development";
+  const production = options.production === true || env === "production";
+  const log =
+    options.log || (env === "test" ? Function.prototype : console.error);
 
-  return function errorhandler (err: any) {
-    const output = toOutput(err, production)
-    log(err)
-    return render(req, output)
-  }
+  return function errorhandler(err: any) {
+    const output = toOutput(err, production);
+    log(err);
+    return render(req, output);
+  };
 }
 
 /**
  * Boom-compatible output.
  */
 interface Output {
-  statusCode: number
-  headers: Record<string, string | string[]>
-  payload: object
+  status: number;
+  headers: Record<string, string | string[]>;
+  payload: object;
 }
 
 /**
  * Convert an error into an "output" object.
  */
-function toOutput (err: any, production: boolean): Output {
-  const output = err.output || {}
-  const statusCode = Number(output.statusCode || err.statusCode) || 500
-  const headers = output.headers || err.headers || {}
+function toOutput(err: any, production: boolean): Output {
+  const output = err.output || {};
+  const status =
+    Number(output.statusCode || err.statusCode || err.status) || 500;
+  const headers = output.headers || err.headers || {};
   const payload = output.payload || {
-    statusCode,
-    error: STATUS_CODES[statusCode] || 'Error',
-    message: (production ? undefined : err.message) || 'Error'
-  }
+    status,
+    error: STATUS_CODES[status] || "Error",
+    message: (production ? undefined : err.message) || "Error"
+  };
 
-  return { statusCode, headers, payload }
+  return { status, headers, payload };
 }
 
 /**
  * Render HTTP response.
  */
-function render (req: Request, output: Output) {
-  const negotiator = new Negotiator({ headers: req.allHeaders.asObject() })
-  const type = negotiator.mediaType(['text/html', 'application/json'])
+function render(req: Request, output: Output) {
+  const negotiator = new Negotiator({ headers: req.headers.asObject() });
+  const type = negotiator.mediaType(["text/html", "application/json"]);
 
-  if (type === 'text/html') return renderHtml(req, output)
+  if (type === "text/html") return renderHtml(req, output);
 
-  return renderJson(req, output)
+  return renderJson(req, output);
 }
 
 /**
  * Render HTML response.
  */
-function renderHtml (req: Request, output: Output) {
-  const content = escapeHtml(JSON.stringify(output.payload, null, 2))
+function renderHtml(req: Request, output: Output) {
+  const content = escapeHtml(JSON.stringify(output.payload, null, 2));
 
-  return sendHtml(req, `
+  return sendHtml(
+    req,
+    `
 <!doctype html>
 <html lang="en">
 <head><meta charset="utf-8"><title>Error</title></head>
 <body>
-<pre>${content.replace(DOUBLE_SPACE_REGEXP, ' &nbsp;')}</pre>
+<pre>${content.replace(DOUBLE_SPACE_REGEXP, " &nbsp;")}</pre>
 </body>
 </html>
-  `.trim(), {
-    skipEtag: true,
-    statusCode: output.statusCode,
-    headers: {
-      'X-Content-Type-Options': 'nosniff',
-      'Content-Security-Policy': "default-src 'self'",
-      ...output.headers
+  `.trim(),
+    {
+      status: output.status,
+      headers: {
+        "X-Content-Type-Options": "nosniff",
+        "Content-Security-Policy": "default-src 'self'",
+        ...output.headers
+      }
     }
-  })
+  );
 }
 
 /**
  * Send JSON response.
  */
-function renderJson (req: Request, output: Output) {
+function renderJson(req: Request, output: Output) {
   return sendJson(req, output.payload, {
-    skipEtag: true,
-    statusCode: output.statusCode,
+    status: output.status,
     headers: {
-      'X-Content-Type-Options': 'nosniff',
+      "X-Content-Type-Options": "nosniff",
       ...output.headers
     }
-  })
+  });
 }
